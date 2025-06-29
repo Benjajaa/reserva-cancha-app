36b95befe1273b98c767dea0386dc00ee911057c
Wenjaja <benjaminsalas934@gmail.com>
2025-06-29 17:13:05 -0400

    fix: retornar version

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,11 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { home } from '../pages/Home';
+
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
     <button
-      onClick={() => logout({ returnTo: home})}
+      onClick={() => logout({ returnTo: window.location.origin })}
       style={styles.button}
     >
       Cerrar sesión
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,6 +1,3 @@
-// frontend/src/router/AppRouter.jsx (Versión Corregida)
-
-// =====> CAMBIO 1: La importación ahora es directa desde el paquete
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from "../pages/Home";
@@ -11,7 +8,6 @@ import Reserva from "../pages/Reserva";
 
 const AppRouter = () => {
   return (
-    // =====> CAMBIO 2: Se usa "BrowserRouter" en lugar de "Router"
     <BrowserRouter>
       <Navbar />
       <Routes>
@@ -20,7 +16,7 @@ const AppRouter = () => {
         <Route path="/implementos"element={<ReservaImplemento />} />
         <Route path="/reserva/:id" element={<Reserva />} />
       </Routes>
-    </BrowserRouter> // <===== Y se cierra con el mismo nombre
+    </BrowserRouter>
   );
 };
 
